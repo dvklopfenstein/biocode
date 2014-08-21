8db3b0665d6cae1acb317ce5fe2e406c76eac972
dklopfenstein <dklopfenstein@users.noreply.github.com>
2014-08-21 00:54:41 -0400

    Added docstrings for each member function.

--- PyDkBio/UCSC/CytoBandBase.py
+++ PyDkBio/UCSC/CytoBandBase.py
@@ -31,7 +31,7 @@
 # Klopfenstein D.V. (2014) Storage and Python Access to UCSC's Cytoband Data In A Species-Independent Manner (Version 2.0) [Computer program]. Available at https://github.com/dklopfenstein/biocode/blob/master/PyDkBio/UCSC/CytoBandBase.py (Accessed [NN Month 20NN])
 
 class CytoBandBase:
-  """Python Interface to UCSC cytoBandIdeo.txt information."""
+  """Python Interface to Species-Independent UCSC cytoBandIdeo.txt information."""
 
   def __init__(self, data):
     """Initialize variables using data downloaded from a UCSC Cytoband file."""
@@ -49,9 +49,11 @@ class CytoBandBase:
     self.stalks  = data['stalks']
 
   def get_sChr_list(self):
+    """Return dictionary with list index as the key and value as commonly known Chromosome name."""
     return [sChr for sChr in self.chr_i2s]
 
   def get_gvarL(self, idx):
+    """Return the length of the gvar areas on a chromosome, if they exist."""
     if self.gvars[idx] is not None:
       L = 0
       for E in self.gvars[idx]:
@@ -61,6 +63,7 @@ class CytoBandBase:
       return None
 
   def get_stalkL(self, idx):
+    """Return the length of the stalk areas on a chromosome, if they exist."""
     if self.stalks[idx] is not None:
       L = 0
       for E in self.stalks[idx]:
@@ -69,7 +72,9 @@ class CytoBandBase:
     else:
       return None
 
-  def get_lens(self, idx): return self.lengths
+  def get_lens(self, idx): 
+    """Return a list containing the length of each chromosome."""
+    return self.lengths
 
   def get_len(self, idx):
     """Get the length of one particular chromosome."""
@@ -77,16 +82,19 @@ class CytoBandBase:
       return self.lengths[idx]
     
   def get_iChr(self, sChr):
+    """Given the commonly known chromosome name, return the chr list index used by this class."""
     if sChr in self.chr_s2i:
       return self.chr_s2i[sChr]
     return None
 
   def get_sChr(self, idx):
+    """Given a chromosome list index, return the commonly known chromosome name."""
     if idx is not None and idx < self.num_chr:
       return self.chr_i2s[idx]
     raise Exception("\n**\n** FATAL: CANNOT DETERMINE CHR NAME WITH ({})\n**\n".format(idx))
 
   def cen_get_len(self, iChr): 
+    """Return the length of the centromere, given a chromosome list index."""
     if iChr < self.num_chr and  \
       self.centromeres[iChr] is not None  and \
       self.centromeres[iChr]['p'] is not None and \
@@ -95,6 +103,7 @@ class CytoBandBase:
     return None
 
   def cen_get_start(self, iChr): 
+    """Return the start base pair of the centromere, given a chromosome list index."""
     if iChr < self.num_chr and \
       self.centromeres[iChr] is not None and \
       self.centromeres[iChr]['q'] is not None:
@@ -102,6 +111,7 @@ class CytoBandBase:
     return None
 
   def cen_get_end(self, iChr): 
+    """Return the end base pair of the centromere, given a chromosome list index."""
     if iChr < self.num_chr and \
       self.centromeres[iChr] is not None and \
       self.centromeres[iChr]['q'] is not None: 

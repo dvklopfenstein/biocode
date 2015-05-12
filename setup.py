0a403000d1ec42b5faab15a82996834c90845978
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2015-05-12 10:43:44 -0400

    Adding files for PyPI

--- setup.py
+++ setup.py
@@ -1,7 +1,26 @@
-# From:
-# http://stackoverflow.com/questions/17583443/what-is-the-correct-way-to-share-package-version-with-setup-py-and-the-package
+from setuptools import setup
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ]
 
+exec(open("goatools/version.py").read())
 setup(
-  name    = 'PyDkBio',
-  version = 1.0.0,
-)
+    name="PyDkBio",
+    packages=['PyDkBio'],
+    version='0.1',
+    description='Python scripts used in my research',
+    author='DV Klopfenstein',
+    author_email='music_pupil@yahoo.com',
+    scripts=['./PyDkBio/NCBI/cols.py']
+    license='BSD',
+    classifiers=classifiers,
+    url='http://github.com/dvklopfenstein/biocode',
+    download_url='http://github.com/dvklopfenstein/biocode/tarball/0.1',
+    description="Python scripts to find enrichment of GO terms",
+    keywords=['NCBI']
+    )

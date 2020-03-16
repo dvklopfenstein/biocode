c993a5ebec74179af46ee4773d1173a02b800a09
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2020-03-16 06:59:45 -0400

    Print hex colors in text

--- src/Tests/matplotlib/colorset_hexvals.py
+++ src/Tests/matplotlib/colorset_hexvals.py
@@ -15,20 +15,24 @@ from pydvkbiology.matplotlib.ColorObj import MplColorHelper
 import numpy as np
 
 
-def run(palette='Set1', num_vals=10):
+#def run(palette='Set1', num_vals=10):
+def run(palette='hsv', num_vals=12):
     """Creating hexvalues for discrete colors using a colormap."""
-    fout_png = 'colors_{P}.png'.format(P=palette)
+    fout_png = 'colors_{P}_{N}.png'.format(P=palette, N=num_vals)
     _, axis = plt.subplots(1, 1, figsize=(6, 6))
     xvals = [10]*num_vals
     yvals = range(num_vals)
     colobj = MplColorHelper(palette, 0, num_vals-1)
     colors = [colobj.get_hexstr(yval) for yval in yvals]
-    for xval, yval, color in zip(xvals, yvals, colors):
+    for idx, (xval, yval, color) in enumerate(zip(xvals, yvals, colors)):
         plt.scatter(xval, yval, s=1000, marker='s', color=color)
         plt.text(xval+.004, yval, color, fontsize=20, va='center')
+    for idx, color in enumerate(reversed(colors)):
+        print('{N:2} {COLOR}'.format(N=idx, COLOR=color))
     axis.set_title('{N} Discrete Colors from {MAP}'.format(N=num_vals, MAP=palette))
     plt.show()
     plt.savefig(fout_png)
+    print('  WROTE: {PNG}'.format(PNG=fout_png))
 
 
 def cli():
